6b42a95e853e4167c7a63a9a71bbd617f5cf929d
Neil Agarwal <regalstreak@gmail.com>
2018-10-01 22:25:41 +0530

    innovinc: Random backup lmao rip

--- src/components/HomeView/HomeView.vue
+++ src/components/HomeView/HomeView.vue
@@ -1,39 +1,63 @@
 <template>
-	<v-container fluid>
-		<v-slide-y-transition mode="out-in">
-			<v-layout column align-center>
-				<img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-				<blockquote>
-					&#8220;First, solve the problem. Then, write the code.&#8221;
-					<footer>
-						<small>
-							<em>&mdash;John Johnson</em>
-						</small>
-					</footer>
-				</blockquote>
-			</v-layout>
-		</v-slide-y-transition>
-	</v-container>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column>
+        <v-carousel>
+          <v-carousel-item
+              v-for="(item,i) in items"
+              :key="i"
+              :src="item.src"
+              reverse-transition="slide-x-transition"
+              transition="slide-y-transition"
+          ></v-carousel-item>
+        </v-carousel>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+          }
+        ]
+      }
+    },
+  };
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
+  h1,
+  h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
+
 </style>
